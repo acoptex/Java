27d49f491a235505fa1314fea5e1d01ce9d73e42
ACuchriajev <info.acoptex@gmail.com>
2020-06-22 20:55:20 +0300

    Geekbrains Lesson1

---
+++ Geekbrains_JavaQuickStart/Lesson1_hello_world/src/acoptex/lesson_1/hello_world/helloWorld.java
@@ -0,0 +1,9 @@
+package acoptex.lesson_1.hello_world;
+
+public class helloWorld {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello, World!");
+        System.out.println("Я буду изучать Java!");
+    }
+}
